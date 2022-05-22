0a63e10ae5ab7571a86b44c4035af94054b9e892
Anton Skripnik <anton.skripnik@gmail.com>
2022-05-22 17:12:02 +0300

    Initial Commit

---
+++ ThymeTraque/ThymeTraqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThymeTraqueApp.swift
+//  ThymeTraque
+//
+//  Created by Anton Skripnik on 22.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ThymeTraqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
