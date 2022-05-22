55757ae0cc49754a9a0251b9913f377df5686690
Anton Skripnik <anton.skripnik@gmail.com>
2022-05-22 19:39:36 +0300

    Create mock design of the TrackView

--- ThymeTraque/Features/Track/View/TrackView.swift
+++ ThymeTraque/Features/Track/View/TrackView.swift
@@ -8,9 +8,39 @@
 import SwiftUI
 
 struct TrackView: View {
+    @State private var editingText: Bool = false
+    @FocusState private var textFieldFocused: Bool
+    @State private var textFieldText: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .tabItem(constructTabItemLabel)
+        VStack {
+            Spacer()
+            
+            VStack {
+                Text("00:00")
+                    .font(.system(size: 70.0, weight: .ultraLight, design: .rounded))
+                
+                Button {
+                    print("Button tapped")
+                } label: {
+                    Image(systemName: "stop.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .frame(width: 240.0)
+            
+            Spacer()
+            
+            TextField("Tap here to edit your activity description", text: $textFieldText)
+                .multilineTextAlignment(.center)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+        }
+        .tabItem(constructTabItemLabel)
     }
     
     func constructTabItemLabel() -> some View {
@@ -23,6 +53,11 @@ struct TrackView: View {
 
 struct TrackView_Previews: PreviewProvider {
     static var previews: some View {
-        TrackView()
+        Group {
+            TrackView()
+                .preferredColorScheme(.light)
+            TrackView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
