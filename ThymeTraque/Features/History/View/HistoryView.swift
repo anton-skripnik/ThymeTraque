c0a77b0f24522050707c07a1992ddddd9eb823c9
Anton Skripnik <anton.skripnik@gmail.com>
2022-05-22 20:03:35 +0300

    Add mock History screen UI

--- ThymeTraque/Features/History/View/HistoryView.swift
+++ ThymeTraque/Features/History/View/HistoryView.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+struct HistoryEntry: Equatable, Identifiable {
+    let id: UUID = UUID()
+    var description: String
+    var timeInterval: TimeInterval
+}
+
 struct HistoryView: View {
+    let entries: Array<HistoryEntry> = [
+        .init(description: "My activity 5", timeInterval: 0.33),
+        .init(description: "My activity 4", timeInterval: 1.33),
+        .init(description: "My activity 3", timeInterval: 2.33),
+        .init(description: "My activity 2", timeInterval: 3.33),
+        .init(description: "My activity 1", timeInterval: 4.33),
+    ]
+    
+    let formatter: DateComponentsFormatter = {
+        let f = DateComponentsFormatter()
+        f.allowedUnits = [.minute, .second]
+        f.zeroFormattingBehavior = .pad
+        return f
+    }()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .tabItem(constructTabItemLabel)
+        NavigationView {
+            List {
+                ForEach(entries) { entry in
+                    HStack {
+                        Text(entry.description)
+                        Spacer()
+                        Text(formatter.string(from: entry.timeInterval) ?? "Unknown")
+                            .foregroundColor(.gray)
+                    }
+                }
+                .onDelete { indexes in
+                    print("Delete")
+                }
+            }
+            .navigationTitle("My activities")
+            .listStyle(.plain)
+        }
+        .tabItem(constructTabItemLabel)
     }
     
     func constructTabItemLabel() -> some View {
