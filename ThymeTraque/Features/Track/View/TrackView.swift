1a415bc05ed6b77cd0b066dad5410311f5cb7c04
Anton Skripnik <anton.skripnik@gmail.com>
2022-05-24 15:35:35 +0300

    Create a layout for Track screen for compact height

--- ThymeTraque/Features/Track/View/TrackView.swift
+++ ThymeTraque/Features/Track/View/TrackView.swift
@@ -12,6 +12,31 @@ struct TrackView: View {
     let store: TrackStore
     let environment: TrackEnvironment
     
+    @Environment(\.verticalSizeClass) var verticalSizeClass
+    
+    var body: some View {
+        Group {
+            if verticalSizeClass == .regular {
+                RegularVerticalTrackViewLayout(store: store, environment: environment)
+            } else {
+                CompactVerticalTrackViewLayout(store: store, environment: environment)
+            }
+        }
+        .tabItem(constructTabItemLabel)
+    }
+    
+    func constructTabItemLabel() -> some View {
+        VStack {
+            Image(systemName: "timer")
+            Text("Track")
+        }
+    }
+}
+
+struct RegularVerticalTrackViewLayout: View {
+    let store: TrackStore
+    let environment: TrackEnvironment
+    
     var body: some View {
         WithViewStore(store) { viewStore in
             VStack {
@@ -48,15 +73,51 @@ struct TrackView: View {
                 Spacer()
             }
         }
-        .tabItem(constructTabItemLabel)
     }
-    
-    func constructTabItemLabel() -> some View {
-        VStack {
-            Image(systemName: "timer")
-            Text("Track")
+}
+
+struct CompactVerticalTrackViewLayout: View {
+    let store: TrackStore
+    let environment: TrackEnvironment
+
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack {
+                Spacer()
+                
+                HStack {
+                    Text(viewStore.activityTimeIntervalString)
+                        .font(.system(size: 70.0, weight: .ultraLight, design: .rounded))
+                        .padding()
+                    
+                    Button {
+                        viewStore.send(.toggleTracking)
+                    } label: {
+                        Image(systemName: viewStore.isTracking ? "stop.circle" : "play.circle")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    }
+                    .frame(height: 100.0)
+                }
+                
+                Spacer()
+                
+                TextField(
+                    "Tap here to add description",
+                    text: viewStore.binding(
+                        get: \.activityDescription,
+                        send: { .activityDescriptionChanged($0) }
+                    )
+                )
+                .multilineTextAlignment(.center)
+                .font(.body)
+                .padding()
+                
+                Spacer()
+            }
         }
     }
+    
 }
 
 struct TrackView_Previews: PreviewProvider {
@@ -66,13 +127,19 @@ struct TrackView_Previews: PreviewProvider {
                 store: .preview,
                 environment: .preview
             )
-                .preferredColorScheme(.light)
-            
-            TrackView(
-                store: .preview,
-                environment: .preview
-            )
-                .preferredColorScheme(.dark)
+            .previewInterfaceOrientation(.landscapeRight)
+
+//            TrackView(
+//                store: .preview,
+//                environment: .preview
+//            )
+//            .preferredColorScheme(.light)
+//
+//            TrackView(
+//                store: .preview,
+//                environment: .preview
+//            )
+//            .preferredColorScheme(.dark)
         }
     }
 }
