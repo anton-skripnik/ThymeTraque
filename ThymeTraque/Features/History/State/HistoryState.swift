2746486b52edb816a3cde9244345ff08c920949a
Anton Skripnik <anton.skripnik@gmail.com>
2022-05-25 11:43:16 +0300

    Remove test entries from the initial live HistoryState

--- ThymeTraque/Features/History/State/HistoryState.swift
+++ ThymeTraque/Features/History/State/HistoryState.swift
@@ -15,13 +15,7 @@ struct HistoryState: Equatable {
 extension HistoryState {
     static let live = HistoryState(
         entries: IdentifiedArrayOf(
-            uncheckedUniqueElements: [
-                .init(id: 1, activityDescription: "My activity 5", timeInterval: 0.33),
-                .init(id: 2, activityDescription: "My activity 4", timeInterval: 1.33),
-                .init(id: 3, activityDescription: "My activity 3", timeInterval: 2.33),
-                .init(id: 4, activityDescription: "My activity 2", timeInterval: 3.33),
-                .init(id: 5, activityDescription: "My activity 1", timeInterval: 4.33),
-            ],
+            uncheckedUniqueElements: [],
             id: \.id
         )
     )
